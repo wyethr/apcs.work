3e4957ccada6b6c2f5ab564a76e3b3f693134602
wzrenwick <wyethrenwick24@marlborough.org>
2021-09-17 14:56:40 -0700

    Update PrimeChallenge.java

--- Unit 1/PrimeChallenge.java
+++ Unit 1/PrimeChallenge.java
@@ -3,20 +3,19 @@ public class PrimeChallenge {
   public static boolean find(int a) {
 
     for (int i = 2; i > a; i++) {
-      if(a%i == 0)
-        return false;
+      if(a%i == 0) {
+        return false; }
+      else {
+        return true; }
     }
 
   }
 
   public static void main(String[] args) {
     for (int i = 2; i > 400; i++) {
-      find(i);
-
-      if(PrimeChallenge.isPrime == true) {
+      if(find(i)) {
         System.out.println(i);
       }
     }
   }
-
 }
