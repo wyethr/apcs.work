4272e6c7d563bc96d207708ebffe4fb4a9af05f1
wzrenwick <wyethrenwick24@marlborough.org>
2021-09-21 08:55:59 -0700

    GOT PRIMECHALLENGE TO WORK! WAHOO!

--- Unit 1/PrimeChallenge.java
+++ Unit 1/PrimeChallenge.java
@@ -1,20 +1,27 @@
 public class PrimeChallenge {
 
   public static boolean find(int a) {
+    int statement = 0;
 
-    for (int i = 2; i > a; i++) {
+    for (int i = 2; i < a; i++) {
       if(a%i == 0) {
-        return false; }
-      else {
-        return true; }
+        statement = 1;
+      }
+    }
+
+    if(statement == 1) {
+      return false;
+    }
+    else {
+      return true;
     }
 
   }
 
   public static void main(String[] args) {
-    for (int i = 2; i > 400; i++) {
-      if(find(i)) {
-        System.out.println(i);
+    for (int b = 2; b < 400; b++) {
+      if(find(b) == true) {
+        System.out.println(b);
       }
     }
   }
