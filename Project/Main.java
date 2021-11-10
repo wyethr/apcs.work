e3f75eae0a52435c81831abd998fc0679a3587c7
wzrenwick <wyethrenwick24@marlborough.org>
2021-11-10 09:58:25 -0800

    Made the thing fill the whole screen

---
+++ Project/Main.java
@@ -0,0 +1,20 @@
+import processing.core.*;
+
+public class Main extends PApplet {
+
+  public void settings() {
+    size(1440, 775);
+  }
+
+  public void setup() {
+    background(0);
+  }
+
+  public void draw() {
+
+  }
+
+  public static void main(String[] args) {
+    PApplet.main("Main");
+  }
+}
